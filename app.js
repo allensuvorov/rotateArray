f4b83e1f05791cf4e26cdc4e77cac06f28341ee0
Allen <allenvs@mail.ru>
2020-08-25 00:57:13 +0300

    added solution0 code

--- app.js
+++ app.js
@@ -3,6 +3,18 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-    
+
+
+// var rotate = function(nums, k) { 
+// };
+
+let nums = [1,2,3,4,5,6,7]
+let k = 3
+
+var rotate = function(array, steps) {
+    // 1st solutions
+    array.unshift(...array.slice(-steps));
+    array.splice(-steps);
 };
+rotate(nums, k);
+console.log(nums);
